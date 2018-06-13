dad7c4d44d6516a37c470a329e5d7986aad2c58b
weronikalidia <weronika.szczypien@gmail.com>
2018-06-13 19:16:47 +0200

    bkn-sliderHp js fix + css close btn

--- src/common/js/app.js
+++ src/common/js/app.js
@@ -3,8 +3,13 @@
 const sliders = document.querySelectorAll('.js-sliderItem');
 
 function activeSlider() {
-    // sliders.forEach(item => item.classList.remove('is-open'));
+    Array.prototype.filter.call(this.parentNode.children, child => {
+        if (child !== this) {
+            child.classList.remove('is-open');
+        }
+    });
     this.classList.toggle('is-open');
+
 }
 
 sliders.forEach(item => item.addEventListener('click', activeSlider));
